f1c64b4ca4f6493e9e3d6b10e79191450e8d5498
Ozge Gokay <>
2017-08-20 18:10:25 +0300

    Second

--- src/main/java/com/jpm/services/DataGenerator.java
+++ src/main/java/com/jpm/services/DataGenerator.java
@@ -1,53 +1,59 @@
 package com.jpm.services;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 import com.jpm.constants.Constant;
 import com.jpm.entities.TradeInstruction;
 import com.jpm.enums.Currency;
 import com.jpm.enums.Type;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class DataGenerator {
 
+	public static Random random = new Random();
 
-    public static Random random = new Random();
+	public static List<TradeInstruction> generateData() {
 
-    public static List<TradeInstruction> generateData(){
+		int numberOfRecords = 2000;
+		List<TradeInstruction> tradeInstructions = new ArrayList<>();
 
-        int numberOfRecords = 2000;
-        List<TradeInstruction> tradeInstructions = new ArrayList<>();
+		for (int i = 1; i <= numberOfRecords; i++) {
+			Currency randomCurrency = Currency.getRandomCurrency();
+			LocalDate settlementDate = getRandomDateFromInstructionDate();
+			LocalDate settlementDateModifiedToWorkDay = DateOperation.setDateToFirstWorkDay(settlementDate, randomCurrency);
+			int randomNumber = random.nextInt(15);
+			String entityName = (randomNumber < 10 ? "0" : "") + randomNumber;
+			TradeInstruction tradeInstruction = TradeInstruction.builder()
+					.entity(Constant.ENTITY_NAME + entityName)
+					.type(Type.getRandomType())
+					.agreedFx(random.nextDouble())
+					.currency(randomCurrency)
+					.instructionDate(getRandomDateFromNowOn())
+					.settlementDate(settlementDate)
+					.unit(random.nextInt(Constant.UNIT))
+					.pricePerUnit(random.nextDouble())
+					.settlementDateModifiedToWorkDay(settlementDateModifiedToWorkDay)
+					.build();
 
-        for (int i = 1; i <= numberOfRecords; i++){
-            Currency randomCurrency = Currency.getRandomCurrency();
-            LocalDate settlementDate = getRandomDateFromInstructionDate();
-            LocalDate settlementDateModifiedToWorkDay = DateOperation.setDateToFirstWorkDay(settlementDate, randomCurrency);
-            int randomNumber = random.nextInt(15);
-            String entityName = (randomNumber < 10 ? "0" : "") + randomNumber;
-            TradeInstruction tradeInstruction = TradeInstruction.builder()
-                    .entity(Constant.ENTITY_NAME + entityName).type(Type.getRandomType()).agreedFx(random.nextDouble())
-                    .currency(randomCurrency).instructionDate(getRandomDateFromNowOn())
-                    .settlementDate(settlementDate)
-                    .unit(random.nextInt(Constant.UNIT)).pricePerUnit(random.nextDouble())
-                    .settlementDateModifiedToWorkDay(settlementDateModifiedToWorkDay)
-                    .build();
+			tradeInstructions.add(tradeInstruction);
+		}
+		return tradeInstructions.stream()
+				.sorted((e1, e2) -> e1.getSettlementDateModifiedToWorkDay().compareTo(e2.getSettlementDateModifiedToWorkDay()))
+				.collect(Collectors.toList());
+	}
 
-            tradeInstructions.add(tradeInstruction);
-        }
-        return tradeInstructions;
-    }
+	public static LocalDate getRandomDateFromNowOn() {
+		return addDaysHoursMinsSecs(LocalDate.now());
+	}
 
-    public static LocalDate getRandomDateFromNowOn(){
-        return addDaysHoursMinsSecs(LocalDate.now());
-    }
+	public static LocalDate getRandomDateFromInstructionDate() {
+		return addDaysHoursMinsSecs(getRandomDateFromNowOn());
+	}
 
-    public static LocalDate getRandomDateFromInstructionDate(){
-        return addDaysHoursMinsSecs(getRandomDateFromNowOn());
-    }
-
-    public static LocalDate addDaysHoursMinsSecs(LocalDate givenDate){
-        return givenDate.plusDays(random.nextInt(Constant.PLUS_DAY));
-    }
+	public static LocalDate addDaysHoursMinsSecs(LocalDate givenDate) {
+		return givenDate.plusDays(random.nextInt(Constant.PLUS_DAY));
+	}
 }
--- src/main/java/com/jpm/services/ReportService.java
+++ src/main/java/com/jpm/services/ReportService.java
@@ -1,100 +1,95 @@
 package com.jpm.services;
 
-import com.jpm.entities.DailyResult;
-import com.jpm.entities.RankResult;
-import com.jpm.entities.TradeInstruction;
-import com.jpm.enums.Type;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingDouble;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.summingDouble;
+import com.jpm.entities.DailyResult;
+import com.jpm.entities.RankResult;
+import com.jpm.entities.TradeInstruction;
+import com.jpm.enums.Type;
 
 public class ReportService {
 
-    public static List<DailyResult> getDailyReport(List<TradeInstruction> tradeInstructions){
-        List<DailyResult> dailyResultList = new ArrayList<>();
+	public static List<DailyResult> getDailyReport(List<TradeInstruction> tradeInstructions) {
+		List<DailyResult> dailyResultList = new ArrayList<>();
 
-        tradeInstructions
-                .stream()
-                .collect(groupingBy(TradeInstruction::getSettlementDateModifiedToWorkDay,
-                         groupingBy(TradeInstruction::getType, summingDouble(TradeInstruction::getAmountInUSD))
-                                    )
-                        )
-                .entrySet().stream().sorted(Comparator.comparing(Map.Entry::getKey))
-                .forEach(x -> {
-                    Map<Type, Double> tmpValue = x.getValue();
-                    double buy = tmpValue.get(Type.BUY) != null ? tmpValue.get(Type.BUY) : 0;
-                    double sell = tmpValue.get(Type.SELL) != null ? tmpValue.get(Type.SELL) : 0;
-                    dailyResultList.add(DailyResult.builder()
-                            .settlementDate(x.getKey())
-                            .totalBuy(buy)
-                            .totalSell(sell)
-                            .build());
-                });
-        return dailyResultList;
-    }
+		tradeInstructions
+				.stream()
+				.collect(groupingBy(TradeInstruction::getSettlementDateModifiedToWorkDay,
+						groupingBy(TradeInstruction::getType, summingDouble(TradeInstruction::getAmountInUSD))))
+				.entrySet()
+				.stream()
+				.sorted(Comparator.comparing(Map.Entry::getKey))
+				.forEach(x -> {
+					Map<Type, Double> tmpValue = x.getValue();
+					double buy = tmpValue.get(Type.BUY) != null ? tmpValue.get(Type.BUY) : 0;
+					double sell = tmpValue.get(Type.SELL) != null ? tmpValue.get(Type.SELL) : 0;
+					dailyResultList.add(DailyResult.builder()
+							.settlementDate(x.getKey())
+							.totalBuy(buy)
+							.totalSell(sell)
+							.build());
+				});
+		return dailyResultList;
+	}
 
-    public static List<RankResult> getRankingReportForBuyOperation(List<TradeInstruction> tradeInstructions) {
+	public static List<RankResult> getRankingReportForBuyOperation(List<TradeInstruction> tradeInstructions) {
 
-        List<RankResult> rankResults = new ArrayList<>();
-        tradeInstructions
-                .stream()
-                .filter(x->x.getType() == Type.BUY)
-                .collect(groupingBy(TradeInstruction::getSettlementDateModifiedToWorkDay,
-                            groupingBy(TradeInstruction::getEntity, summingDouble(TradeInstruction::getAmountInUSD)))
-                        )
-                .entrySet().stream().sorted(Comparator.comparing(Map.Entry::getKey))
-                .forEach(a -> {
-                    int[] rank = {0};
-//                    Comparator c = Map.Entry.comparingByKey().reversed();
-                    //Function<TradeInstruction, Double> byAmountInUSD = y -> y.getAmountInUSD();
-                    a.getValue()
-                            .entrySet()
-                            .stream()
-                            .sorted((e1, e2) -> e1.getValue().compareTo(e2.getValue()))
-                            .forEach(b ->
-                                    rankResults.add(RankResult.builder()
-                                            .settlementDate(a.getKey())
-                                            .rank(++rank[0])
-                                            .entity(b.getKey())
-                                            .amount(b.getValue())
-                                            .build()));
-                });
-        return rankResults;
-    }
+		List<RankResult> rankResults = new ArrayList<>();
+		tradeInstructions
+				.stream()
+				.filter(x -> x.getType() == Type.BUY)
+				.collect(groupingBy(TradeInstruction::getSettlementDateModifiedToWorkDay,
+						groupingBy(TradeInstruction::getEntity, summingDouble(TradeInstruction::getAmountInUSD))))
+				.entrySet()
+				.stream()
+				.sorted(Comparator.comparing(Map.Entry::getKey))
+				.forEach(a -> {
+					int[] rank = { 0 };
+					a.getValue()
+							.entrySet()
+							.stream()
+							.sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+							.forEach(b -> rankResults.add(RankResult.builder()
+									.settlementDate(a.getKey())
+									.rank(++rank[0])
+									.entity(b.getKey())
+									.amount(b.getValue())
+									.build()));
+				});
+		return rankResults;
+	}
 
-    public static List<RankResult> getRankingReportForSellOperation(List<TradeInstruction> tradeInstructions) {
+	public static List<RankResult> getRankingReportForSellOperation(List<TradeInstruction> tradeInstructions) {
 
-        List<RankResult> rankResults = new ArrayList<>();
-        tradeInstructions
-                .stream()
-                .filter(x->x.getType() == Type.SELL)
-                .collect(groupingBy(TradeInstruction::getSettlementDateModifiedToWorkDay,
-                        groupingBy(TradeInstruction::getEntity, summingDouble(TradeInstruction::getAmountInUSD)))
-                )
-                .entrySet().stream().sorted(Comparator.comparing(Map.Entry::getKey))
-                .forEach(a -> {
-                    int[] rank = {0};
-//                    Comparator c = Map.Entry.comparingByKey().reversed();
-                    //Function<TradeInstruction, Double> byAmountInUSD = y -> y.getAmountInUSD();
-                    a.getValue()
-                            .entrySet()
-                            .stream()
-                            .sorted((e1, e2) -> e1.getValue().compareTo(e2.getValue()))
-                            .forEach(b ->
-                                    rankResults.add(RankResult.builder()
-                                            .settlementDate(a.getKey())
-                                            .rank(++rank[0])
-                                            .entity(b.getKey())
-                                            .amount(b.getValue())
-                                            .build()));
-                });
-        return rankResults;
-    }
+		List<RankResult> rankResults = new ArrayList<>();
+		tradeInstructions
+				.stream()
+				.filter(x -> x.getType() == Type.SELL)
+				.collect(groupingBy(TradeInstruction::getSettlementDateModifiedToWorkDay,
+						groupingBy(TradeInstruction::getEntity, summingDouble(TradeInstruction::getAmountInUSD))))
+				.entrySet()
+				.stream()
+				.sorted(Comparator.comparing(Map.Entry::getKey))
+				.forEach(a -> {
+					int[] rank = { 0 };
+					a.getValue()
+							.entrySet()
+							.stream()
+							.sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+							.forEach(b -> rankResults.add(RankResult.builder()
+									.settlementDate(a.getKey())
+									.rank(++rank[0])
+									.entity(b.getKey())
+									.amount(b.getValue())
+									.build()));
+				});
+		return rankResults;
+	}
 
 }
